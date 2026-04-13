c15ce10b2a15aa2b7316d4b5552ca387d6e74293
Amitkr1311 <1311amitkr@gmail.com>
2026-04-13 16:43:39 +0530

    add common environment variable fallbacks and improve vcl deployment

--- src/jwt_password.ts
+++ src/jwt_password.ts
@@ -1,7 +1,7 @@
 import "./env.ts";
 
-const val = process.env.JWT_PASSWORD;
+const val = process.env.JWT_PASSWORD || process.env.JWT_SECRET;
 if (!val) {
-  throw new Error("JWT_PASSWORD is not defined in environment variables");
+  throw new Error("JWT_PASSWORD or JWT_SECRET is not defined in environment variables");
 }
 export const JWT_PASSWORD = val;
--- src/mongo_url.ts
+++ src/mongo_url.ts
@@ -1,8 +1,8 @@
 import "./env.ts";
 
-const val = process.env.MONGO_URL;
+const val = process.env.MONGO_URL || process.env.MONGODB_URI;
 if (!val) {
-  throw new Error("MONGO_URL is not defined in environment variables");
+  throw new Error("MONGO_URL or MONGODB_URI is not defined in environment variables");
 }
 const MONGO_URL = val;
 export default MONGO_URL;
